c3f018b4f5b59b251e7d454e9e94f822de56ae54
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2025-06-24 22:32:09 -0300

    chore: implementacao da imagem do novo projeto

--- portfolio/src/assets/OrganizarImages.js
+++ portfolio/src/assets/OrganizarImages.js
@@ -4,6 +4,7 @@ import bannerAgencia from './img/imgProjetos/bannerAgencia.jpeg';
 import bannerRestaurante from './img/imgProjetos/banner-Restaurante.jpeg';
 import bannerControleEstoque from './img/imgProjetos/controle-estoque.png';
 import bannerAPI from './img/imgProjetos/bannerAPI.png';
+import bannerSistemaAgenda from './img/imgProjetos/banner-sistemaAgenda.jpeg';
 import FundoDesenvolvimento from './img/imgProjetos/FundoDesenvolvimento.jpg';
 import imgAboutMe from './img/laptop-com-icone-de-codigo.avif';
 
@@ -15,5 +16,6 @@ export const objImagens = {
     projectAgencia: bannerAgencia,
     projectRestaurante: bannerRestaurante,
     projectControleEstoque: bannerControleEstoque,
+    projectSistemaAgenda: bannerSistemaAgenda,
     fundoDesenvolvimento: FundoDesenvolvimento
 }
